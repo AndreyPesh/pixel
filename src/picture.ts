89c8f44d9e28e8561c9c6fce2140a6173eb372f6
Andrei <coursestud@mail.ru>
2023-08-28 21:51:06 +0300

    fix: fix

--- src/picture.ts
+++ src/picture.ts
@@ -6,47 +6,55 @@ import createMappedImage from './helpers/createMappedImage';
 
 const canvas = document.getElementById('canvas1') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-canvas.width = 500;
-canvas.height = 606;
-ctx.drawImage(icon, 0, 0, canvas.width, canvas.height);
 
-const pixels = getPixels(canvas, ctx);
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-const particlesArray: Array<Particle> = [];
-const countParticles = 5000;
-
-const mappedImage = createMappedImage(canvas, pixels);
-
-console.log(mappedImage);
-
-const init = () => {
-  for (let i = 0; i < countParticles; i++) {
-    particlesArray.push(new Particle(canvas, ctx, mappedImage));
-  }
-};
-
-init();
-
-let count = 0;
-const animate = () => {
-  // ctx.drawImage(icon, 0, 0, canvas.width, canvas.height);
-  ctx.globalAlpha = count < 1000 ? 0.5 : 0.05;
-  console.log(count);
+window.addEventListener('load', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  const dx = window.innerHeight < 500 ? 0 : -40;
+  const dy = window.innerHeight < 706 ? 0 : 240;
+  console.log(window.innerHeight);
   
 
-  ctx.fillStyle = 'rgb(0,0,0)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  // ctx.globalAlpha = 0.2;
+  ctx.drawImage(icon, dx, dy);
 
-  for (let i = 0; i < particlesArray.length; i++) {
-    particlesArray[i].update();
-    const alpha = particlesArray[i].speed * 0.3 ? particlesArray[i].speed * 0.3 : 0.05
-    ctx.globalAlpha = alpha;
-    particlesArray[i].draw();
-  }
-  count++;
-  requestAnimationFrame(animate);
-};
+  const pixels = getPixels(canvas, ctx);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-animate();
+  const particlesArray: Array<Particle> = [];
+  const countParticles = 5000;
+
+  const mappedImage = createMappedImage(canvas, pixels);
+
+  // console.log(mappedImage);
+
+  const init = () => {
+    for (let i = 0; i < countParticles; i++) {
+      particlesArray.push(new Particle(canvas, ctx, mappedImage));
+    }
+  };
+
+  init();
+
+  let count = 0;
+  const animate = () => {
+    // ctx.drawImage(icon, 0, 0, canvas.width, canvas.height);
+    ctx.globalAlpha = count < 1000 ? 0.5 : 0.05;
+    // console.log(count);
+
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // ctx.globalAlpha = 0.2;
+
+    for (let i = 0; i < particlesArray.length; i++) {
+      particlesArray[i].update();
+      const alpha =
+        particlesArray[i].speed * 0.3 ? particlesArray[i].speed * 0.3 : 0.05;
+      ctx.globalAlpha = alpha;
+      particlesArray[i].draw();
+    }
+    count++;
+    requestAnimationFrame(animate);
+  };
+
+  animate();
+});
